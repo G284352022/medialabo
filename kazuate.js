14fa7b8572aa95c35ea55ea907614c18117dc2a9
G284352022 <228435@st.takushoku-u.ac.jp>
2023-05-23 22:48:08 +0900

    ページ上の文が変化するように編集

--- kazuate.js
+++ kazuate.js
@@ -5,12 +5,19 @@ console.log('答え（デバッグ用）: ' + kotae);
 // 入力回数（予想回数）
 let kaisu = 0;
 
+//表示位置を検索(回数の更新時に使用)
+let k_posi = document.querySelector('p > span#kaisu');
+
+//表示位置を検索(予想の表示を更新する際に使用)
+let a_posi = document.querySelector('p > span#answer');
+
+//判定結果の表示位置を検索(判定を表示するときに使用)
+let r_posi = document.querySelector('p#result');
+
+
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
 hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
@@ -18,9 +25,16 @@ function hantei() {
     // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
     let yoso = 4;
 
-    kaisu = kaisu + 1;
-    console.log(kaisu + "回目の予想: " + yoso);
+    //回数の表示を変更する
+        //kaisuの更新
+        kaisu = kaisu + 1;
+
+        //表示する要素の内容を変更
+        k_posi.textContent = kaisu;
+    //回数の更新完了
   
+    //予想の表示を更新する
+    a_posi.textContent = yoso;
 
 
     // 課題3-1: 正解判定する
@@ -30,25 +44,25 @@ function hantei() {
             
             // 課題3-1における出力先はコンソール
 
-            console.log("正解です．おめでとう!");
+            r_posi.textContent = '正解です．おめでとう!';
             kaisu = 4;
         }
         else if(kotae < yoso){
             if(kaisu == 3)
-                console.log("まちがい．残念でした答えは " + kotae + " です．");
+                r_posi.textContent = 'まちがい．残念でした答えは ' + kotae + ' です．';
             else
-                console.log("まちがい．答えはもっと小さいですよ");
+                r_posi.textContent = 'まちがい．答えはもっと小さいですよ';
         }
         else{
             if(kaisu == 3)
-                console.log("まちがい．残念でした答えは " + kotae + " です．");
+                r_posi.textContent = 'まちがい．残念でした答えは ' + kotae + ' です．';
             else
-                console.log("まちがい．答えはもっと大きいですよ");
+                r_posi.textContent = 'まちがい．答えはもっと大きいですよ';
         }
 
     }
     else{
-        console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
+        r_posi.textContent = '答えは ' + kotae + ' でした．すでにゲームは終わっています';
     }
 
 
